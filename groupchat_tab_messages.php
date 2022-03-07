022710eeece664a0dd1e2e33ed9c539cf5609b9f
JM <1910598john@gmail.com>
2022-03-07 13:33:35 +0800

    commit

--- groupchat_tab_messages.php
+++ groupchat_tab_messages.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = 'convos';
+$database = 'users';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -14,24 +14,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$chosen = null;
-$user = null;
-if (isset($_SESSION['chosen'])) {
-    $chosen = $_SESSION['chosen'];
-    $user = $_SESSION['myusername'];
-}
-$convo = $chosen.$user;
 $alignment = null;
 $bgc = null;
 $font_color = null;
 
-$sql = "SELECT id, message, sentby FROM ".$convo;
+$sql = "SELECT id, message, sentby FROM groupchat";
 $res = $conn->query($sql);
 
-
-if (!(empty($res))) {
+if ($res->num_rows > 0) {
     while ($row = $res->fetch_assoc()) {
-        if ($row['sentby'] == $chosen) {
+        if ($row['sentby'] != $_SESSION['myusername']) {
             $alignment = 'justify-content: flex-start';
             $bgc = '#E4E6EB';
             $font_color = '#050505';
@@ -41,12 +33,12 @@ if (!(empty($res))) {
             $bgc = '#1877f2';
             $font_color = '#fff';
         }
-        echo '<div id="message'.$row['id'].'" class="message '.$row['sentby'].'" style="display:flex;'.$alignment.';margin:10px">
-                  <div style="max-width:50%;overflow-wrap:break-word;background:'.$bgc.';color:'.$font_color.';padding:10px;border-radius:10px;">'.$row['message'].'</div>
+        echo '<div id="group-message'.$row['id'].'" class="group-message '.$row['sentby'].'" style="display:flex;'.$alignment.';margin:10px">
+                  <div style="max-width:40%;overflow-wrap:break-word;background:'.$bgc.';color:'.$font_color.';padding:7px;border-radius:10px;font-size:12px;">'.$row['message'].'</div>
               </div>';
-        $_SESSION['lastId'] = $row['id'];
     }
 }
+
 
 $conn->close();
 ?>
